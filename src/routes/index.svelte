6d84a3b064a4274e1ad3b9ee453abd900d0e89e4
Ryan Atkinson <mail@ryanatkn.com>
2021-10-14 12:42:26 -0500

    iframe the about page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,11 @@
 
 <main class="column markup">
 	<section>
-		<h1>spiderspace</h1>
+		<h1 class="panel-inset">spiderspace</h1>
 		<img class="pixelated" src="/favicon.png" />
 	</section>
+	<!-- TODO show spinner while iframe is loading -->
+	<iframe class="panel-outset" src="https://spiderspace.github.io/about" />
 </main>
 
 <style>
@@ -20,5 +22,11 @@
 	img {
 		width: 256px;
 		height: 256px;
+	}
+	iframe {
+		border: none;
+		/* TODO hardcoding as a shortcut */
+		width: 380px;
+		height: 180px;
 	}
 </style>
