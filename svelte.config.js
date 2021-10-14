302bff233d00b2d40c6261710df1a3d8802dba0b
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-10-14 11:57:21 -0500

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import {typescript} from 'svelte-preprocess-esbuild';
+import static_adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV !== 'production';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	preprocess: typescript(),
+	kit: {
+		adapter: static_adapter(),
+		target: '#root',
+		paths: dev ? undefined : {base: '/felt-template'}, // for GitHub pages -- delete this line for top-level domains
+		files: {assets: 'src/static'},
+		vite: {
+			ssr: {
+				noExternal: ['@feltcoop/felt'],
+			},
+			optimizeDeps: {
+				exclude: ['@feltcoop/felt'],
+			},
+		},
+	},
+};
