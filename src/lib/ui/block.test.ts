205ec838101e2e2c95d1ad0c5d7fec65852dfe7b
Ryan Atkinson <mail@ryanatkn.com>
2021-12-31 14:10:26 -0600

    parse block test with arrays

--- src/lib/ui/block.test.ts
+++ src/lib/ui/block.test.ts
@@ -6,10 +6,18 @@ import {parseBlock} from '$lib/ui/block';
 /* test__parseBlock */
 const test__parseBlock = suite('parseBlock');
 
-test__parseBlock('basic behavior', () => {
+test__parseBlock('parses data into a block or not', () => {
 	assert.is(parseBlock(null), null);
 	assert.is(parseBlock({}), null);
 	assert.is(parseBlock({type: 'Text'}), null);
+});
+
+// TODO maybe change this API to make `parseBlock` work on arrays?
+// The reasoning to not support this is
+// we don't want to pay the cost of reflection on every `parseBlock` call.
+test__parseBlock('does not parse arrays', () => {
+	assert.is(parseBlock([]), null);
+	assert.is(parseBlock([{type: 'Text'}]), null);
 });
 
 test__parseBlock.run();
