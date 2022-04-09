9e3552632426a7296870e5982195bbac51664fe5
Ryan Atkinson <mail@ryanatkn.com>
2022-04-09 12:48:18 -0500

    format

--- src/lib/ui/BlockView.svelte
+++ src/lib/ui/BlockView.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import {getApp} from '$lib/app/app';
 	import {getDevmode} from '@feltcoop/felt/ui/devmode.js';
-	import type { Block, ElementBlock} from '$lib/ui/block';
+	import type {Block, ElementBlock} from '$lib/ui/block';
 	import BlockViewChildren from '$lib/ui/BlockViewChildren.svelte';
 
 	const {parseOptions} = getApp();
@@ -29,10 +29,10 @@
 		this={parseOptions.components.get(block.component)}
 		{...block.props}
 	/>{:else if block.type === 'Text'}{block.content}{:else if block.type === 'Element'}<svelte:element
-			{...block.attributes}
-			this={block.element}
-			data-entity={block.id}
-			on:click={block.onClick ? toClickHandler(block) : undefined}
-			><!--TODO should not override `target`/`rel` if it's an internal or trusted host-->
-			<BlockViewChildren {block} />
-</svelte:element>{/if}
+		this={block.element}
+		{...block.attributes}
+		data-entity={block.id}
+		on:click={block.onClick ? toClickHandler(block) : undefined}
+		><!--TODO should not override `target`/`rel` if it's an internal or trusted host-->
+		<BlockViewChildren {block} />
+	</svelte:element>{/if}
